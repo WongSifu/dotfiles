6b59860b67228cb2bd5c2ab0f6956569fc7c0c65
Andrew Lim <write2andrewlim@gmail.com>
2024-03-03 00:02:40 -0500

    script to install zsh plugins

---
+++ .config/zsh/zsh_plugin_setup.sh
@@ -0,0 +1,7 @@
+!#/bin/sh
+
+# ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.config/zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.config/zsh/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search.git $HOME/.config/zsh/plugins/zsh-history-substring-search
