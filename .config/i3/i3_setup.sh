217f17ee932807bc802bd574f416ab0e8bc03457
Andrew Lim <write2andrewlim@gmail.com>
2024-03-03 00:31:17 -0500

    adding setup i3 setup

---
+++ .config/i3/i3_setup.sh
@@ -0,0 +1,16 @@
+!#/bin/sh
+
+pacman -S \
+	xorg \
+	lightdm-gtk-greeter \
+	i3-wm \
+	i3lock \
+	i3status \
+	dmenu \
+	kitty \
+	nitrogen \
+	lxappearance \
+	gtk-engine-murrine
+
+# git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git $HOME/Downloads
+# mkdir .themes .icons
