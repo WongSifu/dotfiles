1a64b0c5fdd0ea284a0dee0a47a073ee1784cfe8
Andrew Lim <write2andrewlim@gmail.com>
2024-03-03 01:10:56 -0500

    bug fixes

--- .config/i3/i3_setup.sh
+++ .config/i3/i3_setup.sh
@@ -12,5 +12,8 @@ pacman -S \
 	lxappearance \
 	gtk-engine-murrine
 
+wget -P $HOME/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Lilex.zip
+wget -P $HOME/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/ProFont.zip
+
 # git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git $HOME/Downloads
 # mkdir .themes .icons
